ea95f4f4ce5dd18b07c5170c3b23845e542bce28
Kevin Luo <kvn.luo@gmail.com>
2020-11-26 12:41:33 -0500

    typedefs created

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -1,3 +1,19 @@
 module.exports = `
-
+  type Todo {
+    id: ID!
+    title: String!
+    is_completed: Boolean
+    is_public: Boolean
+    user: User!
+  }
+  type User {
+    id: ID!
+    name: String!
+  }
+  type Query {
+    getTodos: [Todo!]
+  }
+  type Mutation {
+    createTodo(title: String!, is_completed: Boolean, is_public: Boolean, userId: ID): Todo!
+  }
 `
