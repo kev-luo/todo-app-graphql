cb23f239538a742a2cd391ddef2c03b2baa3711a
Kevin Luo <kvn.luo@gmail.com>
2020-11-26 16:17:46 -0500

    strikethrough text if completed, begin todo toggle mutation

--- client/src/components/Todos.js
+++ client/src/components/Todos.js
@@ -8,13 +8,31 @@ export default function Todos({ todo }) {
   const classes = useStyles();
   const [toggleCompleted] = useMutation(UPDATE_TODO_MUTATION);
 
+  const toggleTodo = () => {
+    toggleCompleted({
+      variables: {
+        todoId: todo.id,
+        is_completed: !todo.is_completed,
+      },
+    });
+  };
+
   return (
     <div className={classes.root}>
-      <div>{todo.title}</div>
+      <div className={todo.is_completed ? classes.completed : ""}>
+        {todo.title}
+      </div>
+      <input onClick={toggleTodo} type="checkbox" checked={todo.is_completed} />
     </div>
   );
 }
 
 const useStyles = makeStyles((theme) => ({
-  root: {},
+  root: {
+    display: "flex",
+    justifyContent: "space-between",
+  },
+  completed: {
+    textDecoration: "line-through",
+  },
 }));
