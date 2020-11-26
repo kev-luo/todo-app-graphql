672b3f4a4ce4b7e3cce48ffbfa38e30b295e1659
Kevin Luo <kvn.luo@gmail.com>
2020-11-26 13:18:01 -0500

    front end todos query string and createtodo mutation string

---
+++ client/src/utils/graphql.js
@@ -0,0 +1,21 @@
+import { gql } from '@apollo/client';
+
+export const GET_TODOS_QUERY = gql`
+  {
+    getTodos {
+      id
+      title
+      is_completed
+    }
+  }
+`
+
+export const CREATE_TODO_MUTATION = gql`
+  mutation createTodo($title: String!) {
+    createTodo(title: $title) {
+      id
+      title
+      is_completed
+    }
+  }
+`
